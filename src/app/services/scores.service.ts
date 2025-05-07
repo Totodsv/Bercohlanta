f1911aafdbb244ed5bae82301aa8b94d30852992
Totodsv <53949850+Totodsv@users.noreply.github.com>
2025-05-07 16:46:38 +0200

    cleaning project

--- src/app/services/scores.service.ts
+++ src/app/services/scores.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable, of } from 'rxjs';
-import { Score } from '../models/score.model';
+// import { Observable, of } from 'rxjs';
+// import { Score } from '../models/score.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,48 +13,48 @@ export class ScoresService {
 
   getScores(): any {
     console.log("Bienvenue en", environment);
-    // return this.httpClient.get(`${environment.apiUrl}`);
-    return of([
-      {
-          "Epreuves": "Relais Eau",
-          "Equipe 1": "5",
-          "Equipe 2": "2",
-          "Equipe 3": "4",
-          "Equipe 4": "3",
-          "Equipe 5": "1"
-      },
-      {
-          "Epreuves": "Parcous Combattant",
-          "Equipe 1": "5",
-          "Equipe 2": "1",
-          "Equipe 3": "3",
-          "Equipe 4": "4",
-          "Equipe 5": "2"
-      },
-      {
-          "Epreuves": "A l'aveugle",
-          "Equipe 1": "5",
-          "Equipe 2": "2",
-          "Equipe 3": "3",
-          "Equipe 4": "4",
-          "Equipe 5": "1"
-      },
-      {
-          "Epreuves": "Dégustation",
-          "Equipe 1": "3",
-          "Equipe 2": "5",
-          "Equipe 3": "2",
-          "Equipe 4": "4",
-          "Equipe 5": "1"
-      },
-      {
-          "Epreuves": "Poule renard vipère",
-          "Equipe 1": "5",
-          "Equipe 2": "3",
-          "Equipe 3": "2",
-          "Equipe 4": "4",
-          "Equipe 5": "1"
-      }
-  ])
+    return this.httpClient.get(`${environment.apiUrl}`);
+  //   return of([
+  //     {
+  //         "Epreuves": "Relais Eau",
+  //         "Equipe 1": "5",
+  //         "Equipe 2": "2",
+  //         "Equipe 3": "4",
+  //         "Equipe 4": "3",
+  //         "Equipe 5": "1"
+  //     },
+  //     {
+  //         "Epreuves": "Parcous Combattant",
+  //         "Equipe 1": "5",
+  //         "Equipe 2": "1",
+  //         "Equipe 3": "3",
+  //         "Equipe 4": "4",
+  //         "Equipe 5": "2"
+  //     },
+  //     {
+  //         "Epreuves": "A l'aveugle",
+  //         "Equipe 1": "5",
+  //         "Equipe 2": "2",
+  //         "Equipe 3": "3",
+  //         "Equipe 4": "4",
+  //         "Equipe 5": "1"
+  //     },
+  //     {
+  //         "Epreuves": "Dégustation",
+  //         "Equipe 1": "3",
+  //         "Equipe 2": "5",
+  //         "Equipe 3": "2",
+  //         "Equipe 4": "4",
+  //         "Equipe 5": "1"
+  //     },
+  //     {
+  //         "Epreuves": "Poule renard vipère",
+  //         "Equipe 1": "5",
+  //         "Equipe 2": "3",
+  //         "Equipe 3": "2",
+  //         "Equipe 4": "4",
+  //         "Equipe 5": "1"
+  //     }
+  // ])
   }
 }
